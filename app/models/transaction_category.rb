cc0472e306a07064ad45ebff42c0957eb124b826
Xavier Simó <xsimov@gmail.com>
2024-05-08 15:41:10 +0200

    Remove all references to Kakebo

--- app/models/transaction_category.rb
+++
@@ -1,6 +0,0 @@
-class TransactionCategory < ApplicationRecord
-  belongs_to :parent_category, class_name: 'TransactionCategory', optional: true
-  has_many :children_categories, class_name: 'TransactionCategory', foreign_key: 'parent_category_id'
-
-  scope :final_children, -> { where.not(parent_category_id: nil) }
-end
--- app/views/incomes/_income.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! income, :id, :created_at, :updated_at
-json.url income_url(income, format: :json)
